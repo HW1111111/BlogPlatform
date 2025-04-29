620b232d3aefaad46bd168956330e45a9029e212
Haonan Wu <hw3059@nyu.edu>
2025-04-29 00:04:24 -0400

    Fix submodule issue: reupload blog_admin, blog_api, blog_client as folders

---
+++ blog_api/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  db: {
+    host: 'database-1.ccvym6aw4353.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'asdfASDF',
+    database: 'blog',
+  },
+  token: {
+    secretKey: 'A%&(SD)(AS*DAV^AD%*^VA%)(D^',
+  },
+}
